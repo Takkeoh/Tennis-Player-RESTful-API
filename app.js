feee426b197b52a70b6f1fa89f2fd964a348d473
Paul Bouteiller <paul.bouteiller@epita.fr>
2022-06-19 23:39:50 +0200

    Added seperation between server and app to facilitate testing

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// Base imports
 import express from 'express';
 const app = express();
+// Export for separation between the app and the server
+export default app;
 import { getPlayersDTO, findPlayerDTO, getStatsDTO } from './Presentation/DTO/endpoints.js';
 
 // Base requests
 app.get('/players', getPlayersDTO);
 app.get('/players/:id', findPlayerDTO);
 app.get('/stats', getStatsDTO);
-
-// Launch the server
-app.listen(8080, () => {
-    console.log('Server started.')
-})
---
+++ server.js
@@ -0,0 +1,6 @@
+import app from './app.js'
+
+// Launch the server
+app.listen(8080, () => {
+    console.log('Server started.')
+})
